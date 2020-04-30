d2f63dadfc9965c54bd5e540badcf78f1a39d497
Adam <influenc3@outlook.com>
2020-04-30 23:30:45 +0200

    string

--- src/site/content/en/accessible/accessible.11tydata.js
+++ src/site/content/en/accessible/accessible.11tydata.js
@@ -7,7 +7,7 @@ module.exports = {
     slug: 'accessible',
     cover: '/images/collections/accessible.svg',
     title: 'Dostępność dla wszystkich',
-    description: 'Build a site that works for all of your users.',
+    description: 'Zbuduj stronę dostępną dla wszystkich użytkowników.',
     overview: `All users should be able to access your content. In this section
       you'll learn how to provide a robust experience to your users that
       accounts for their context and any situational, temporary, or permanent
